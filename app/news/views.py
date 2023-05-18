5e71560b25422ed28776ef8b4d2fcaf683b3d902
SunriseFading <sunrisefading@proton.me>
2023-05-18 23:00:32 +0300

    add pagination

--- app/news/views.py
+++ app/news/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import ListView
 from news.models import News
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
-class NewsListView(ListView):
+class NewsListView(LoginRequiredMixin, ListView):
     model = News
     context_object_name = "news"
+    paginate_by = 2
+    login_url = reverse_lazy("account:login")
